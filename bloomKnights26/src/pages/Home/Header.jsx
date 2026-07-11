37160ad8d85d2193cadd3f36b31ad38a415ce07e
joet <205975513+joetmp4@users.noreply.github.com>
2026-07-11 14:47:07 -0400

    fixed log in and header

--- bloomKnights26/src/pages/Home/Header.jsx
+++ bloomKnights26/src/pages/Home/Header.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router"
 import "./Header.css";
 
 function Header() {
+    const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+
     function handleLogout() {
         localStorage.removeItem("isLoggedIn");
         window.location.replace("/login");
@@ -12,9 +14,14 @@ function Header() {
             <h1 className="header__title">Bloom Knights</h1>
             <nav className="header__nav">
                 <Link to="/" className="header__link">Home</Link>
-                <Link to="/signup" className="header__link">Sign Up</Link>
-                <Link to="/login" className="header__link">Login</Link>
-                <Link to="/login" className="header__link" onClick={handleLogout}>Logout</Link>
+                {isLoggedIn ? (
+                    <button className="header__link header__logout" onClick={handleLogout}>Logout</button>
+                ) : (
+                    <>
+                        <Link to="/login" className="header__link">Login</Link>
+                        <Link to="/signup" className="header__link">Sign Up</Link>
+                    </>
+                )}
             </nav>
         </header>
     );
