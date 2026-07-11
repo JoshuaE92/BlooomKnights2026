de4273e4dbfd93131133d4dcd1933e91240d6a29
joet <205975513+joetmp4@users.noreply.github.com>
2026-07-11 17:03:32 -0400

    cart initial commit

---
+++ bloomKnights26/src/pages/Cart.jsx
@@ -0,0 +1,15 @@
+import Header from "./Home/Header";
+import "./Cart.css";
+
+function Cart() {
+	return (
+		<main className="cart-page">
+			<Header />
+			<div className="cart-content">
+				<h1>Cart</h1>
+			</div>
+		</main>
+	);
+}
+
+export default Cart;
